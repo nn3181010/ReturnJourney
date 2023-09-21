2a6898924e9b0461b9b384602da534a646f3420e
Naveen Purnakanti <118908164+nn3181010@users.noreply.github.com>
2023-09-21 11:52:07 +0530

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000; // You can choose any port you like
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/mydb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Define User Schema and Model
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String, // You should hash the password using bcrypt
+  phoneNumber: String,
+  otp: String,
+});
+
+const UserModel = mongoose.model('User', userSchema);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
