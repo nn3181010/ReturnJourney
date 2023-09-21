8629eaa5414fd7b143f22d80a9996712d44e5da8
Naveen Purnakanti <118908164+nn3181010@users.noreply.github.com>
2023-09-21 11:53:03 +0530

    ipinfo.js

--- ipinfo.js
+++ ipinfo.js
@@ -1,1 +1,21 @@
+const axios = require('axios');
+
+// Validate user's IP address
+async function validateIPAddress(ip) {
+  try {
+    const response = await axios.get(`https://ipinfo.io/${ip}/json?token=YOUR_API_KEY`);
+    const data = response.data;
+
+    // You can perform validation based on IP information here
+    const countryCode = data.country;
+    const city = data.city;
+
+    return { countryCode, city };
+  } catch (error) {
+    console.error('Error validating IP address:', error);
+    return null;
+  }
+}
+
+module.exports = { validateIPAddress };
 
